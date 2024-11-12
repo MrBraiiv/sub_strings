56eb77860e2a0988adc9fde19581a0a73c27f93b
Mr braiiv <mikam6285@gmail.com>
2024-11-12 19:12:53 +0200

    program ready

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,35 +1,18 @@
-random_words = [
-  "apple", "banana", "kiwi", "mango", "strawberry", "watermelon", "cherry",
-  "peach", "grape", "orange", "pineapple", "plum", "fig", "lemon", "lime",
-  "blueberry", "raspberry", "apricot", "melon", "nectarine", "pomegranate",
-  "coconut", "guava", "papaya", "passionfruit", "dragonfruit", "lychee",
-  "avocado", "blackberry", "cranberry", "gooseberry", "jackfruit", "durian",
-  "persimmon", "cantaloupe", "mulberry", "tangerine", "starfruit", "quince",
-  "date", "olive", "pumpkin", "carrot", "broccoli", "cucumber", "lettuce",
-  "tomato", "potato", "zucchini", "radish", "spinach", "parsley", "garlic",
-  "onion", "ginger", "thyme", "basil", "cilantro", "mint", "oregano",
-  "rosemary", "sage", "chili", "pepper", "turmeric", "cabbage", "celery",
-  "cherry", "walnut", "pecan", "almond", "hazelnut", "chestnut", "cashew",
-  "pistachio", "macadamia", "peanut", "sunflower", "chia", "flax", "sesame",
-  "corn", "wheat", "barley", "oats", "rye", "rice", "quinoa", "buckwheat",
-  "sorghum", "teff"
-]
-require 'pry-byebug'
-
 def substrings(string, dictionary)
   found_strings =
     dictionary.select do |str|
       string.include?(str)
     end
   
-    # p found_strings
-
-  test = found_strings.reduce(Hash.new(0)) do |count, str|
-    # binding.pry
-    count[str] += 1
-    count
+  count = found_strings.reduce(Hash.new(0)) do |result, str|
+    cnt = string.scan(str).count
+    next if cnt == 0
+    result[str] = cnt
+    result
   end
-  p test
+  p count
 end
 
-substrings("apple kiwi apple", random_words)
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
